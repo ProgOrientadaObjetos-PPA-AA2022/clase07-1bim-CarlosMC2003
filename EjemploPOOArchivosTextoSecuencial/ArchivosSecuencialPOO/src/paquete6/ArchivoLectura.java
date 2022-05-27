10da882b3bb507b71925346d2689485935996236
CarlosMC2003 <carlitosenrike@gmail.com>
2022-05-26 21:50:41 -0500

    Clase07 Avance

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/ArchivoLectura.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/ArchivoLectura.java
@@ -5,10 +5,93 @@
  */
 package paquete6;
 
-/**
- *
- * @author reroes
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArchivoLectura {
-    
+
+    private Scanner entrada;
+    private String nombreArchivo;
+    private String rutaArchivo;
+    private ArrayList<Hospital> lista;
+
+    public ArchivoLectura(String n) {
+        nombreArchivo = n;
+        rutaArchivo = String.format("data/%s", nombreArchivo);
+        File f = new File(rutaArchivo);
+        if (f.exists()) {
+            try {
+                entrada = new Scanner(new File(rutaArchivo));
+            } 
+            catch (FileNotFoundException e) {
+                System.err.println("Error al leer del archivo: " + e);
+
+            }
+        }
+    }
+
+    public void establecerNombreArchivo(String n) {
+        nombreArchivo = n;
+    }
+
+    public void establecerRutaArchivo() {
+        rutaArchivo = String.format("data/%s.txt",
+                obtenerNombreArchivo());;
+    }
+
+    public String obtenerNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    public String obtenerRutaArchivo() {
+        return rutaArchivo;
+    }
+
+    public void establecerLista() {
+        lista = new ArrayList<>();
+        File f = new File(rutaArchivo);
+
+        if (f.exists()) {
+
+            while (entrada.hasNext()) {
+                String linea = entrada.nextLine();
+
+                ArrayList<String> linea_partes = new ArrayList<>(Arrays.asList(linea.split(";")));
+                
+                Hospital hp = new Hospital(linea_partes.get(0),linea_partes.get(1), linea_partes.get(2));
+                lista.add(hp);
+
+            } 
+        }
+    }
+
+    public ArrayList<Hospital> obtenerLista() {
+
+        return lista;
+    }
+
+    public void cerrarArchivo() {
+        if (entrada != null) {
+            entrada.close();
+        } // cierra el archivo
+
+    }
+
+    @Override
+    public String toString() {
+        String cadena = "Lista de Hospitales\n";
+        for (int i = 0; i < obtenerLista().size(); i++) {
+            Hospital hpTemporal = obtenerLista().get(i); // Obj. Profesor
+            cadena = String.format("%s(%d) %s - %d - %.2f\n", cadena,
+                    i + 1,
+                    hpTemporal.obtenerNombre(), // obtenerLista().get(i).obtenerNombre(),
+                    hpTemporal.obtenerNumeroCamas(),
+                    hpTemporal.obtenerPresupuesto());    // obtenerLista().get(i).obtenerTipo());
+        }
+        return cadena;
+    }
 }
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
@@ -10,5 +10,29 @@ package paquete6;
  * @author reroes
  */
 public class Ejecutor {
+    public static void main(String[] args) {
+        String nombreArchivo = "hospitales.txt";
+        
+        Hospital hp1 = new Hospital("Isidro Ayora", 35, 1200.00);
+        Hospital hp2 = new Hospital("Clinica Abendaño", 17, 2875.00);
+        Hospital hp3 = new Hospital("Manuel Agustin", 50, 20000.75);
+        
+        ArchivoEscritura archivo = new ArchivoEscritura(nombreArchivo);
+        
+        archivo.establecerRegistro(hp1);
+        archivo.establecerSalida();
+        
+        archivo.establecerRegistro(hp2);
+        archivo.establecerSalida();
+            
+        archivo.establecerRegistro(hp3);
+        archivo.establecerSalida();
+        
+        ArchivoLectura lectura = new ArchivoLectura(nombreArchivo);
+        lectura.establecerLista();
+        
+        System.out.println(lectura);
+        lectura.cerrarArchivo();
+    }
     
 }
